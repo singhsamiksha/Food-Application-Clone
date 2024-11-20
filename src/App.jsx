be96d60dfbdb01ab93c794941ac5ac13c33b8f30
Samiksha <singhsamiksha877@gmail.com>
2024-11-20 14:56:51 +0530

    changes are applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './Components/header.jsx';
 import Carousel from './Components/carousel.jsx';
+import Restaurant from './Components/restaurant.jsx';
 
 function App() {
    return (
       <>
       <Header />
       <Carousel/>
+      <Restaurant/>
       </>
    );
 }
